4f933a820708b7bfb9ac509566c31cce36fb9394
Andreas Geiges <andreas.geiges@climateanalytics.org>
2020-06-10 16:01:53 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -58,7 +58,9 @@ def main():
             "pint==0.9",
             "pycountry",
             "fuzzywuzzy",
-            "tqdm"],
+            "tqdm"
+            "pyam-iamc<=0.3.0",
+            "openpyxl"],
         }
     rtn = setup(**setup_kwargs)
 
