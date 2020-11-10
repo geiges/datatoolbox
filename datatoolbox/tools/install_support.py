63d86dd04383c696ccb26bf95e87cbae87e09f6d
Andreas Geiges <andreas.geiges@climateanalytics.org>
2020-11-10 09:14:40 +0100

    fixed creation of initial config

--- datatoolbox/tools/install_support.py
+++ datatoolbox/tools/install_support.py
@@ -38,13 +38,9 @@ def create_personal_setting(modulePath, OS):
         userName = input("Please enter your initials")
         file_path_variable = input("Please enter path to datashelf")
     
-    if not file_path_variable.endswith('/'):
-            file_path_variable = file_path_variable + '/'
-    print ("\nfile_path_variable = ", file_path_variable)
-    
-    fin = open(os.path.join([modulePath, 'data','personal_template.py']), 'r')
-    os.makedirs(os.path.join([modulePath, 'settings']),exist_ok=True)
-    fout = open(os.path.join([modulePath, 'settings','personal.py']), 'w')
+    fin = open(os.path.join(modulePath, 'data','personal_template.py'), 'r')
+    os.makedirs(os.path.join(modulePath, 'settings'),exist_ok=True)
+    fout = open(os.path.join(modulePath, 'settings','personal.py'), 'w')
     
     for line in fin.readlines():
         outLine = line.replace('XX',userName).replace('/PPP/PPP', file_path_variable)
@@ -55,9 +51,9 @@ def create_personal_setting(modulePath, OS):
 
 def create_initial_config(modulePath):
     import git
-    fin = open(os.path.join([modulePath, 'data','personal_template.py']), 'r')
-    os.makedirs(os.path.join([modulePath, 'settings']),exist_ok=True)
-    fout = open(os.path.join([modulePath, 'settings','personal.py']), 'w')
+    fin = open(os.path.join(modulePath, 'data','personal_template.py'), 'r')
+    os.makedirs(os.path.join(modulePath, 'settings'),exist_ok=True)
+    fout = open(os.path.join(modulePath, 'settings','personal.py'), 'w')
     
     DEBUG = False
     READ_ONLY = True
